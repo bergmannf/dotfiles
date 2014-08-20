0426052e04587a80cfaca1c3a3f35743a9b9f725
Florian Bergmann <Bergmann.F@gmail.com>
2014-08-20 22:21:54 +0200

    Changed emms setup to include mpd information.

--- .emacs.d/setup-emms.el
+++ .emacs.d/setup-emms.el
@@ -3,3 +3,11 @@
 (emms-standard)
 
 (emms-default-players)
+
+(require 'emms-player-mpd)
+(setq emms-player-mpd-server-name "127.0.0.1")
+(setq emms-player-mpd-server-port "6600")
+(add-to-list 'emms-info-functions 'emms-info-mpd)
+(add-to-list 'emms-player-list 'emms-player-mpd)
+
+(require 'emms-browser)
