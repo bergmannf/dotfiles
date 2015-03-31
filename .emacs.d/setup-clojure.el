acbee9a6aea8449093a3abc74e05443c19dde6fa
Florian Bergmann <Bergmann.F@gmail.com>
2015-03-31 23:16:03 +0200

    More ignored clojure-modes where evil makes no sense.
    Signed-off-by: Florian Bergmann <Bergmann.F@gmail.com>

--- .emacs.d/setup-clojure.el
+++ .emacs.d/setup-clojure.el
@@ -4,6 +4,7 @@
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
 (add-to-list 'evil-emacs-state-modes 'cider-stacktrace-mode)
+(add-to-list 'evil-emacs-state-modes 'cider-test-report-mode)
 (add-hook 'cider-doc-mode-hook 'evil-emacs-state)
 
 (setq cider-repl-print-length 100)
