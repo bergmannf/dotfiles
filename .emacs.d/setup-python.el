6abd8e2aae94fcbac8d262ace1d49ee599372bf1
Florian Bergmann <Bergmann.F@gmail.com>
2015-09-17 11:22:38 +0200

    Setup anaconda-mode for python development.

--- .emacs.d/setup-python.el
+++ .emacs.d/setup-python.el
@@ -15,3 +15,10 @@
       "';'.join(module_completion('''%s'''))\n"
       python-shell-completion-string-code
       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'eldoc-mode)
+
+(eval-after-load "company"
+ '(progn
+   (add-to-list 'company-backends 'company-anaconda)))
