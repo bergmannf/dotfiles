95bfef0672e94f8062ab65a85b6bffd145d5336f
Florian Bergmann <Bergmann.F@gmail.com>
2015-10-15 23:11:43 +0200

    Add indentation and alignment to haskell setup.

--- .emacs.d/setup-haskell.el
+++ .emacs.d/setup-haskell.el
@@ -12,8 +12,30 @@
   (add-to-list 'exec-path my-cabal-path))
 
 (custom-set-variables '(haskell-tags-on-save t))
+(custom-set-variables '(company-ghc-show-info t))
+(custom-set-variables '(haskell-indent-ind))
 
 (require 'company)
 
 (add-to-list 'company-backends 'company-ghc)
-(custom-set-variables '(company-ghc-show-info t))
+(add-hook 'haskell-mode-hook (lambda () (electric-indent-local-mode -1)))
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(require 'align)
+
+(add-to-list 'align-rules-list
+             '(haskell-types
+               (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+               (modes quote (haskell-mode literate-haskell-mode))))
+(add-to-list 'align-rules-list
+             '(haskell-assignment
+               (regexp . "\\(\\s-+\\)=\\s-+")
+               (modes quote (haskell-mode literate-haskell-mode))))
+(add-to-list 'align-rules-list
+             '(haskell-arrows
+               (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+               (modes quote (haskell-mode literate-haskell-mode))))
+(add-to-list 'align-rules-list
+             '(haskell-left-arrows
+               (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+               (modes quote (haskell-mode literate-haskell-mode))))
