0d5ebf47a1c9bb56943f934857b74a5910d5fc10
Florian Bergmann <Bergmann.F@gmail.com>
2015-10-17 13:19:35 +0200

    Automove windows to workspaces.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,7 +11,7 @@ import XMonad.Actions.CycleWS(swapNextScreen)
 import System.IO
 
 
-myWorkspaces = ["Web", "Dev", "Mus", "Vid", "Gam", "Read", "7", "8", "9"]
+myWorkspaces = ["1:Web", "2:Dev", "3:Mus", "4:Vid", "5:Gam", "6:Read", "7", "8", "9"]
 myTerminal = "gnome-terminal"
 myNormalBorderColor = "#263238"
 myFocusedBorderColor = "#82B1FF"
@@ -23,7 +23,14 @@ myStartUp = do
   spawnOnce "xrandr --output 'DVI-1' --mode 1920x1080 --primary --output 'DVI-0' --mode 1280x1024 --left-of 'DVI-1'"
 
 myManageHook = composeAll
-               [ isFullscreen --> doFullFloat
+               [ className =? "Firefox" --> doShift "1:Web"
+               , className =? "Iceweasel" --> doShift "1:Web"
+               , className =? "Emacs" --> doShift "2:Dev"
+               , className =? "Cantata" --> doShift "3:Mus"
+               , className =? "vlc" --> doShift "4:Vid"
+               , className =? "Steam" --> doShift "5:Gam"
+               , className =? "libprs500" --> doShift "6:Read"
+               , isFullscreen --> doFullFloat
                ]
 
 main :: IO ()
