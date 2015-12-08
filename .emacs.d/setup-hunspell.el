4c699ad0c3bb1f9fc07255b9d342e81d7ea33160
Florian Bergmann <fbergmann@suse.de>
2015-12-08 10:24:13 +0100

    Simplify hunspell setup.

--- .emacs.d/setup-hunspell.el
+++ .emacs.d/setup-hunspell.el
@@ -1,21 +1,5 @@
 (require 'ispell)
-(add-to-list 'ispell-dictionary-alist '("deutsch-hunspell"
-                                              "[[:alpha:]]"
-                                              "[^[:alpha:]]"
-                                              "[']"
-                                              t
-                                              ("-d" "de_DE"); Dictionary file name
-                                              nil
-                                              iso-8859-1))
 
-(add-to-list 'ispell-dictionary-alist '("english-hunspell"
-                                              "[[:alpha:]]"
-                                              "[^[:alpha:]]"
-                                              "[']"
-                                              t
-                                              ("-d" "en_US")
-                                              nil
-                                              iso-8859-1))
+(setq ispell-program-name (executable-find "hunspell"))
 
-(setq ispell-program-name "hunspell"          ; Use hunspell to correct mistakes
-      ispell-dictionary   "deutsch-hunspell") ; Default dictionary to use
+(setq ispell-dictionary "en_US")
