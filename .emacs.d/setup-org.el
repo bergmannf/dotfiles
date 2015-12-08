7c6d124d9d1e6acaaebc71ee0b60de12d68a5647
Florian Bergmann <Bergmann.F@gmail.com>
2015-12-08 18:56:04 +0100

    Merged.

--- .emacs.d/setup-org.el
+++ .emacs.d/setup-org.el
@@ -5,6 +5,15 @@
 (defun add-pcomplete-capf ()
   (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
 
+(setq org-agenda-files
+      (delq nil
+            (mapcar (lambda (x) (and (file-exists-p x) x))
+                    '("~/Dropbox/notes/overview.org"
+                      "~/Dropbox/notes/junggesellenabschied/overview.org"
+                      "~/Dropbox/notes/rpg/overview.org"
+                      "~/Dropbox/notes/haskell/overview.org"
+                      "~/Dropbox/notes/scala/overview.org"))))
+
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 (add-hook 'org-mode-hook 'add-pcomplete-capf)
 
@@ -13,9 +22,9 @@
 
 (global-set-key (kbd "C-c c") 'org-capture)
 
-(global-set-key (kbd "C-c o")
-                (lambda () (interactive)
-                  (find-file "~/Documents/org/organizer.org")))
+(global-set-key (kbd "C-c o") 
+                (lambda () (interactive) (find-file "~/Dropbox/notes/overview.org")))
 
-(setq org-default-notes-file "~/Documents/org/organizer.org")
+(setq org-default-notes-file "~/Dropbox/notes/overview.org")
+
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
