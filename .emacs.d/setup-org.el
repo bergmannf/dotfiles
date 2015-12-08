fc7a55d16d11f6aa94eab7a3710b3effe191346b
Florian Bergmann <fbergmann@suse.de>
2015-12-08 10:43:24 +0100

    add more org setup

--- .emacs.d/setup-org.el
+++ .emacs.d/setup-org.el
@@ -1,3 +1,21 @@
 (require 'ox-reveal)
 
 (require 'htmlize)
+
+(defun add-pcomplete-capf ()
+  (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t))
+
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+(add-hook 'org-mode-hook 'add-pcomplete-capf)
+
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(global-set-key (kbd "C-c o")
+                (lambda () (interactive)
+                  (find-file "~/Documents/org/organizer.org")))
+
+(setq org-default-notes-file "~/Documents/org/organizer.org")
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
