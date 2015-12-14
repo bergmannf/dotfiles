47ab4763fb5835a32b5a729fb5a9de20c456710e
Florian Bergmann <fbergmann@suse.de>
2015-12-14 12:57:54 +0100

    Cleanup haskell-setup.

--- .emacs.d/setup-haskell.el
+++ .emacs.d/setup-haskell.el
@@ -9,11 +9,7 @@
 
 (add-to-list 'evil-emacs-state-modes 'haskell-interactive-mode)
 (add-to-list 'evil-emacs-state-modes 'haskell-error-mode)
-
-(custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
+(add-to-list 'evil-emacs-state-modes 'haskell-presentation-mode)
 
 (eval-after-load 'haskell-mode '(progn
   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
@@ -22,7 +18,8 @@
   (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)))
 
 (eval-after-load 'haskell-cabal '(progn
   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
@@ -34,9 +31,16 @@
   (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path))
 
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
 (custom-set-variables '(haskell-tags-on-save t))
 (custom-set-variables '(company-ghc-show-info t))
 (custom-set-variables '(haskell-indent-ind))
+(custom-set-variables '(haskell-process-use-presentation-mode t))
+;; Requires hasktags executable in PATH
+(custom-set-variables '(haskell-tags-on-save t))
 
 (require 'company)
 
