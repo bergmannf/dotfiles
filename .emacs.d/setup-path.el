d92c64672657b522e53d22915ce6924bb19e97b4
Florian Bergmann <fbergmann@suse.de>
2016-02-09 10:17:18 +0100

    Add $HOME/bin to PATH in emacs.

--- .emacs.d/setup-path.el
+++ .emacs.d/setup-path.el
@@ -1,3 +1,7 @@
 (let ((my-local-bin-path (expand-file-name "~/.local/bin")))
   (setenv "PATH" (concat my-local-bin-path path-separator (getenv "PATH")))
   (add-to-list 'exec-path my-local-bin-path))
+
+(let ((my-home-bin-path (expand-file-name "~/bin")))
+  (setenv "PATH" (concat my-home-bin-path path-separator (getenv "PATH")))
+  (add-to-list 'exec-path my-home-bin-path))
