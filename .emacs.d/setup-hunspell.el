7df4c55a9cfb6fac06eeb4d6328d6cc19fe9869f
Florian Bergmann <fbergmann@suse.de>
2016-02-19 12:12:10 +0100

    Use flyspell in all modes:
    - flyspell-mode in text-modes
    - flyspell-prog-mode in programming-modes.

--- .emacs.d/setup-hunspell.el
+++ .emacs.d/setup-hunspell.el
@@ -3,3 +3,6 @@
 (setq ispell-program-name (executable-find "hunspell"))
 
 (setq ispell-dictionary "en_US")
+
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
